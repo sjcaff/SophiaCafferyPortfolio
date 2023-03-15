7c914517854d11503db129efdae5bf5d3f740535
sjcaff <sjcaffery01@gmail.com>
2023-03-15 15:37:09 -0400

    buttons and colors

---
+++ app.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('.section');
+const sBtns = document.querySelectorAll('.controls');
+const sBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main');
+
+function pageTrans(){
+    //button active class 
+    for (let i = 0; i < sBtn.length; i++)
+    {
+        
+    }
+}
